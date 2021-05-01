3c465656ef82dbfe7e3de41ebee11b126d7a8074
Jackson Santos <56548381+jacksuh@users.noreply.github.com>
2021-05-01 20:11:22 -0300

    Create project

---
+++ src/main/java/com/connectedIn/connectedapi/ConnectedapiApplication.java
@@ -0,0 +1,13 @@
+package com.connectedIn.connectedapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectedapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectedapiApplication.class, args);
+	}
+
+}
