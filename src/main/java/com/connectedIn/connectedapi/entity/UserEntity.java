143577180ac6d86b5959d71a6f7b3923a001fd69
Daniel Albino <daniel.albino@gmail.com>
2021-05-03 18:08:34 -0300

    creating add user flow (2)

---
+++ src/main/java/com/connectedIn/connectedapi/entity/UserEntity.java
@@ -0,0 +1,24 @@
+package com.connectedIn.connectedapi.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String lastName;
+    private String email;
+    private String username;
+    private String password;
+    private LocalDate born;
+}
